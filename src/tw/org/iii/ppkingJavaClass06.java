b84f9b08548970d114986431cc34c91ec3cf16e3
test <test@test_iii>
2016-10-31 15:49:40 +0800

    Class Lesson

--- src/tw/org/iii/ppkingJavaClass06.java
+++ src/tw/org/iii/ppkingJavaClass06.java
@@ -3,6 +3,7 @@ package tw.org.iii;
 public class ppkingJavaClass06 {
     public static void main(String[] pptest){
         int a = 10 , b = 3 ;
+        //& 會前後都做出結果再去比較 && 只要前面條件不符合就跳出
         if (++a <= 10 && b-- >=3){
             System.out.println("OK:a="+a+",b = "+ b);
         }
