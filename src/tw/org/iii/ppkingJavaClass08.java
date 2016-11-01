0174e7341a43814f504ecc0b80369c75ebfb915e
test <test@test_iii>
2016-11-01 09:23:08 +0800

    Class Lesson 161101

---
+++ src/tw/org/iii/ppkingJavaClass08.java
@@ -0,0 +1,13 @@
+package tw.org.iii;
+
+
+public class ppkingJavaClass08 {
+    public static  void main(String[] ppTest){
+        int i=0;
+        //for (首次執行敘述句;執行前的判斷;執行後的敘述句)
+        for (System.out.println("ppking") ; i<10 ;
+             System.out.println("------")){
+            System.out.println(i++);
+        }
+    }
+}
